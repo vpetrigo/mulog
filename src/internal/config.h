8abec33c5fafabb10374c525605bba0db76726b9
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-09-28 03:25:29 +0200

    mulog: config: Update configuration implementation

--- src/internal/config.h
+++ src/internal/config.h
@@ -7,8 +7,24 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define MULOG_ENABLE_TIMESTAMP (MULOG_INTERNAL_ENABLE_COLOR_OUTPUT) /**< Add timestamp to the log line */
-#define MULOG_ENABLE_COLOR     (MULOG_INTERNAL_ENABLE_TIMESTAMP_OUTPUT) /**< Enable color log lines */
-#define MULOG_OUTPUT_HANDLERS  (MULOG_INTERNAL_OUTPUT_HANDLERS) /**< Maximum number of output handlers that can be registered */
+/**
+ * \brief Flag that specify whether to add timestamp to the log line
+ */
+#define MULOG_ENABLE_TIMESTAMP (MULOG_INTERNAL_ENABLE_COLOR_OUTPUT)
+
+/**
+ * \brief Flag that specify whether to enable color log lines
+ */
+#define MULOG_ENABLE_COLOR (MULOG_INTERNAL_ENABLE_TIMESTAMP_OUTPUT)
+
+/**
+ * \brief Maximum number of output handlers that can be registered
+ */
+#define MULOG_OUTPUT_HANDLERS (MULOG_INTERNAL_OUTPUT_HANDLERS)
+
+/**
+ * \brief Log line termination
+ */
+#define MULOG_LOG_LINE_TERMINATION "\n"
 
 #endif /* CONFIG_H */
