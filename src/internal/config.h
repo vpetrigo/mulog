55f355567b369284a6d4d315b6fec89fa53d949f
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-10-06 17:09:27 +0200

    mulog: config: Update configuration
    Added:
    - timestamp getter function
    - thread lock/unlock functions

--- src/internal/config.h
+++ src/internal/config.h
@@ -7,15 +7,23 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
 /**
  * \brief Flag that specify whether to add timestamp to the log line
  */
-#define MULOG_ENABLE_TIMESTAMP (MULOG_INTERNAL_ENABLE_COLOR_OUTPUT)
+#define MULOG_ENABLE_TIMESTAMP (MULOG_INTERNAL_ENABLE_TIMESTAMP_OUTPUT)
+
+#define MULOG_ENABLE_LOCKING (0)
 
 /**
  * \brief Flag that specify whether to enable color log lines
  */
-#define MULOG_ENABLE_COLOR (MULOG_INTERNAL_ENABLE_TIMESTAMP_OUTPUT)
+#define MULOG_ENABLE_COLOR (MULOG_INTERNAL_ENABLE_COLOR_OUTPUT)
 
 /**
  * \brief Maximum number of output handlers that can be registered
@@ -26,5 +34,28 @@
  * \brief Log line termination
  */
 #define MULOG_LOG_LINE_TERMINATION "\n"
+
+/**
+ * \brief External function that is used for getting time value
+ * \return Current system time value in milliseconds
+ */
+extern unsigned long mulog_config_mulog_timestamp_get(void);
+
+/**
+ * \brief External function that is used for locking logger in a multi-thread environment
+ * \return Status of the lock operation
+ * \retval true Lock has been successfully acquired
+ * \retval false Lock has not been acquired
+ */
+extern bool mulog_config_mulog_lock(void);
+
+/**
+ * \brief External function that is used for unlocking logger in a multi-thread environment
+ */
+extern void mulog_config_mulog_unlock(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* CONFIG_H */
