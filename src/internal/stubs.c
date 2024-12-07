2891f853e87b25847eba9987bc74db3fa07a8ad9
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-12-07 11:13:23 +0100

    mulog: stubs: Add mulog stubs to src/internal directory
    Introduce basic stub implementations for `mulog_config_mulog_lock` and `mulog_config_mulog_unlock` functions.

---
+++ src/internal/stubs.c
@@ -0,0 +1,18 @@
+/**
+ * \file
+ * \brief Mulog stubs
+ * \author Vladimir Petrigo
+ */
+
+#include <stdbool.h>
+
+// PUBLIC FUNCTION DEFINITIONS
+
+bool mulog_config_mulog_lock(void)
+{
+    return true;
+}
+
+void mulog_config_mulog_unlock(void)
+{
+}
